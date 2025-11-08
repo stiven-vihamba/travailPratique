9061c0e4b6d1d63d4997cfebb3d6971a68133d43
Stiven <stivenvihamba96@gmail.com>
2025-11-08 06:55:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Script pour la soumission du formulaire de contact
 document.getElementById('contact-form').addEventListener('submit', function(e) {
   e.preventDefault();
   alert('Message envoyé avec succès!');
